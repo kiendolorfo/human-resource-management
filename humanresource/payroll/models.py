d36376ef935d8b5cee33db354c9604cc8fab9673
Kien Dolorfo <37242789+kee-en@users.noreply.github.com>
2023-03-25 19:32:45 +0800

    HRM-4 Add Employee Table

--- humanresource/payroll/models.py
+++ humanresource/payroll/models.py
@@ -10,7 +10,8 @@ class Employee(models.Model):
     hire_date = models.DateField()
     job_title = models.CharField(max_length=50)
     department = models.CharField(max_length=50)
-    manager = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
-    
+    manager = models.ForeignKey(
+        'self', null=True, blank=True, on_delete=models.SET_NULL)
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
